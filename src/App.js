51beb02aa5abce1fb5bfb810401fd0ab22cf5417
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-10 00:00:02 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -3,25 +3,22 @@ import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
 import Nav from "./Nav";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Main from "./Main";
+import Search from "./Search";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Banner />
+        <Switch>
+          <Route path="/search" component={Search} exact />
+          <Route path="/" component={Main} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
